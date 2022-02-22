7fd2c27df8523b3a11e3cd0e67a835a7b91e2351
Sandy Instructor <sandyludosky@gmail.com>
2022-02-22 11:08:22 +0100

    03_04_f

--- fichiers-cours/1.2-prototype_pattern/index.js
+++ fichiers-cours/1.2-prototype_pattern/index.js
@@ -1,12 +1,15 @@
 var Task = function (name) {
   this.name = name;
-  this.toString = function () {
-    console.log("I have to " + this.name);
-  };
 };
+
+Task.prototype.toString = function () {
+  console.log("I have to " + this.name);
+};
+
 
 var task1 = new Task("learn javascript design patterns");
 var task2 = new Task("create new class");
 
+console.log(task1)
 task1.toString();
 task2.toString();
