7e2bd0b12c2449c871ef6100fae5fc394e62299a
Sandy Instructor <sandyludosky@gmail.com>
2022-02-22 14:14:44 +0100

    03_08_f

---
+++ fichiers-cours/1.5-module_pattern/index.js
@@ -0,0 +1,18 @@
+class Task {
+  constructor(name) {
+    this.name = name;
+    this.completed = false;
+  }
+  complete() {
+    this.completed = !this.completed;
+  }
+  toString() {
+    console.log("Task : " + this.name + ", completed:" + this.completed);
+  }
+}
+
+var task1 = new Task("learn javascript design patterns");
+
+task1.toString();
+task1.complete();
+task1.toString();
