96d005cec42626ce2cbb24424cd605393007096f
Sandy Instructor <sandyludosky@gmail.com>
2022-02-24 14:07:49 +0100

    05_06_d

---
+++ fichiers-cours/3.3-observer_pattern/index.js
@@ -0,0 +1,19 @@
+var Member = function (name) {
+  this.name = name;
+  this.chatroom = null;
+};
+
+class Subject {
+  constructor() {
+    this.observers = [];
+  }
+  subscribe(fn) {}
+  unsuscribe(fn) {}
+  notify(object) {}
+}
+
+// subject
+
+
+
+// observers
