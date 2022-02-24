6f18f9e166550ce27242d563f04750892ece31e4
Sandy Instructor <sandyludosky@gmail.com>
2022-02-24 12:35:48 +0100

    05_02_f

--- fichiers-cours/3.1-mediator_pattern/index.js
+++ fichiers-cours/3.1-mediator_pattern/index.js
@@ -15,28 +15,44 @@ var Member = function (name) {
   };
 };
 
+Member.prototype.send = function(message) {
+  this.chatgroup.send(this, message);
+}
+
+Member.prototype.receive = function(...args) {
+  const [sender, message] = args[0];
+  console.log("sender " + sender.name + " message:" + message);
+}
+
+Member.prototype.notification = function(message) {
+  console.log(message)
+}
+
 class ChatGroup {
   constructor(name) {
     this.name = name;
     this.members = {};
   }
 
-  add = function (member) {
+  add = function(member) {
     this.members[member.name] = member;
     member.chatgroup = this;
     Object.keys(this.members).forEach((key) => {
-      this.members[key].notification(`new member ${member.name}`);
-    });
-  };
+      if(key !== member.name) {
+        this.members[key].notification("new member " + member.name);
+      }
+    })
+  }
+
   // broadcast message to all members of the group chat
-  send = function (...args) {
-    const [sender] = args;
+  send = function(...args) {
+    const [sender] =args;
     Object.keys(this.members).forEach((key) => {
-      if (key !== sender.name) {
+      if(key !== sender.name) {
         this.members[key].receive(args);
       }
-    });
-  };
+    })
+  }
 }
 
 var chatroom = new ChatGroup("Family & Friends");
@@ -49,3 +65,8 @@ chatroom.add(member2);
 chatroom.add(member3);
 
 member1.send("Hey all, what are you doing today ?");
+
+
+
+  
+
